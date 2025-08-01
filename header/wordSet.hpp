7550089f672f19db9226fdc2c77288dd579e153e
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-08-01 21:27:35 +0000

    add restrictions

--- header/wordSet.hpp
+++ header/wordSet.hpp
@@ -2,32 +2,53 @@
 #include <string>
 #include <algorithm>
 #include <map>
-
+#include <stdexcept>
 
 class Word{
+    private:
     std::string w;
     int size;
 
     Word(std::string word, int s): w(word), size(s){
-      //impl  
+      if(word.length()==0||word.length()>6){
+        throw std::invalid_argument("Tamanho da palavra deve ser entre 1 e 6");
+      }
     }
+
+    public:
+
+    std::string seeWord(){
+        return w;
+    }
+
+    int size(){
+        return size;
+    }
+
 
 };
 
 class Def{
+    private:
     std::string def;
     int size;
 
     Def(std::string d, int s): def(d), size(s){
-        //impl
+        if(def.length()==0||def.length()>50){
+            throw std::invalid_argument("Descrição precisa ter, no máximo, 50 caracteres");
+        }
     }
+
+    public:
+
 };
 
-/*
-herda de Def e de Word
-class WordDef{
+
+//herda de Def e de Word
+class WordDef : public Def, public Word{
     std::map<Word, Def> wd;
 
 
 };
-*/
+
+
