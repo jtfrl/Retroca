d790c642767b26de726b969eb20cf10a6dc54394
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-08-04 15:15:17 +0000

    add

--- header/wordSet.hpp
+++ header/wordSet.hpp
@@ -5,48 +5,65 @@
 #include <stdexcept>
 
 class Word{
-    private:
-    std::string w;
-    int size;
+    protected:
+        std::string w;
+        int size;
 
-    Word(std::string word, int s): w(word), size(s){
-      if(word.length()==0||word.length()>6){
-        throw std::invalid_argument("Tamanho da palavra deve ser entre 1 e 6");
-      }
-    }
+        Word(std::string word, int s): w(word), size(s){
+            if(word.length()==0||word.length()>6){
+                throw std::invalid_argument("Tamanho da palavra deve ser entre 1 e 6");
+            }
+        }
 
     public:
 
-    std::string seeWord(){
-        return w;
-    }
+        std::string seeWord(){
+            return w;
+        }
 
-    int size(){
-        return size;
-    }
+        int size(){
+            return size;
+        }
 
 
 };
 
 class Def{
-    private:
-    std::string def;
-    int size;
+    protected:
+        std::string def;
+        int size;
 
-    Def(std::string d, int s): def(d), size(s){
-        if(def.length()==0||def.length()>50){
-            throw std::invalid_argument("Descrição precisa ter, no máximo, 50 caracteres");
+        //Def(std::string d, int s);
+
+        Def(std::string d, int s): def(d), size(s){
+            if(def.length()==0||def.length()>50){
+                throw std::invalid_argument("Descrição precisa ter, no máximo, 50 caracteres");
+            }
         }
-    }
 
     public:
+
+        std::string seeDef(){
+            return def;
+        }
+
+        int size(){
+            return size;
+        }
 
 };
 
 
 //herda de Def e de Word
 class WordDef : public Def, public Word{
+    private:
     std::map<Word, Def> wd;
+
+    WordDef(std::map<Word, Def> WD): wd(WD){
+        //definir palavras e definição aqui
+    }
+
+    public:
 
 
 };
